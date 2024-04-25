9567eb64e2cc514a0ee5acc65d6bcf9ae0786479
Shebin-shaji <shebinshaji2615@gmail.com>
2024-04-25 20:59:12 +0530

    initial commit

---
+++ lib/countryList.dart
@@ -0,0 +1,27 @@
+final List<Map<String, dynamic>> countryLst = [
+  {
+    'name': 'India',
+    'img':
+        'https://static.toiimg.com/thumb/msid-92089121,width-748,height-499,resizemode=4,imgsize-139308/.jpg'
+  },
+  {
+    'name': 'Germany',
+    'img':
+        'https://www.travelanddestinations.com/wp-content/uploads/2023/04/Cochem-in-Germany.jpg'
+  },
+  {
+    'name': 'Italy',
+    'img':
+        'https://blog.thomascook.in/wp-content/uploads/2017/09/Colosseum_in_Rome_Italy_-_April_2007-Medium-300x176.jpg'
+  },
+  {
+    'name': 'America',
+    'img':
+        'https://www.shutterstock.com/image-photo/statue-liberty-over-scene-new-600nw-1476328154.jpg'
+  },
+  {
+    'name': 'Germany',
+    'img':
+        'https://a.cdn-hotels.com/gdcs/production133/d1829/743b2f60-bb25-11e8-a095-0242ac11000d.jpg?impolicy=fcrop&w=800&h=533&q=mediumhttps://a.cdn-hotels.com/gdcs/production133/d1829/743b2f60-bb25-11e8-a095-0242ac11000d.jpg?impolicy=fcrop&w=800&h=533&q=medium'
+  },
+];
---
+++ lib/model.dart
@@ -0,0 +1,12 @@
+import 'package:tourist_app/countryList.dart';
+
+
+class Country {
+  String name;
+  String img;
+
+  Country({required this.name, required this.img});
+}
+
+List<Country> countries =
+    countryLst.map((e) => Country(name: e['name'], img: e['img'])).toList();
